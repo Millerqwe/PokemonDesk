297cb3e7db990cf57417a7dfd7c2bd32ef358fb3
Millerqwe <millerqwe@gmail.com>
2020-10-31 18:27:08 +0300

    Add homework 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import cn from 'classnames';
 
 import './App.css';
 import style from './App.modules.scss';
 
 
-const App = () => {
+export const App: FunctionComponent = () => {
   return (
     <div className={cn(style.text, 'text--opacity')}>
       There is a app component
     </div>
   );
 };
-
-export default App;
